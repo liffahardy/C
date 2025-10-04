6dbf0fe80e49ae58e190bbbd2228d024317dab03
ardy <102403141+liffahardy@users.noreply.github.com>
2025-10-04 20:07:20 +0700

    Add files via upload
    arithmetic operators = + - * / % ++ --

---
+++ main3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+
+    // arithmetic operators = + - * / % ++ --
+
+    int x = 10;
+    int y = 2;
+    int z = 0;
+
+    // z = x + y;
+    // z = x - y;
+    // z = x * y;
+    // z = x / y;
+    // z = x % y;
+
+    // x++;
+    // x--;
+
+    // x = x + 2;
+    // x+=3;
+    // x-=3;
+    // x *= 2;
+    x /= 2;
+
+    printf("%d", x);
+
+    return 0;
+
+}
